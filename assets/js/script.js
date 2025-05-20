91c9e47443bd73198d3ac4d5576f4222be44677f
chanwinharold <153543115+chanwinharold@users.noreply.github.com>
2025-05-20 22:22:03 +0000

    project updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,131 +1,51 @@
-/**Fonctions pour vérifier la validité de chaque
- * champ du formulaire après l'action submit
- */
+const form = document.querySelector("form")
+const popup = document.querySelector('dialog')
 
-function verifyFirstName(firstName) {
-    let errorState = document.getElementById("error-first-name-input")
-    let classe = document.getElementById("firstName")
-    let regex = new RegExp("^([a-zA-Z])+$");
-    let firstNameTested = regex.test(firstName);
-    if (firstName.trim() === "") {
-        errorState.innerHTML = `This field is required`
-        classe.style.border = '1px solid red';
-    } else if (firstNameTested === false) {
-        errorState.innerHTML = `Please enter a valid first name`
-        classe.style.border = '1px solid red';
-    } else {
-        classe.style.border = '1px solid green';
-        return true
+form.addEventListener('submit', (e)=> {
+    e.preventDefault()
+
+    const firstname = document.getElementById('firstName')
+    const lastname = document.getElementById('lastName')
+    const email = document.getElementById('email')
+    const radios = document.querySelectorAll('input[name="queryType"]')
+    const message = document.getElementById('message')
+    const checkbox = document.getElementById('checkbox')
+
+    document.querySelectorAll('.error').forEach((span) => span.innerText = "")
+    
+    let valid = 0
+
+    if (firstname.value.trim() == "") {
+        document.querySelector('#firstName + span').innerText = "This field cannot be empty"
+    } else valid++
+
+    if (lastname.value.trim() == "") {
+        document.querySelector('#lastName + span').innerText = "This field cannot be empty"
+    } else valid++
+
+    if (email.value.trim() == "") {
+        document.querySelector('#email + span').innerText = "This field cannot be empty"
+    } else if (!email.checkValidity()) {
+        document.querySelector('#email + span').innerText = "Please enter a valid email"
+    } else valid++
+
+    if (!radios[0].checked && !radios[1].checked) {
+        document.querySelector('.radio-container + span').innerText = "Please choose a query type"
+    } else valid++
+
+    if (message.value.trim() == "") {
+        document.querySelector('#message + span').innerText = "This field cannot be empty"
+    } else valid++
+
+    if (!checkbox.checked) {
+        document.querySelector('label[for="checkbox"] > span').innerText = "Please check this field"
+    } else valid++
+
+    if (valid ==6) {
+       popup.showModal()
     }
-}
+})
 
-function verifyLastName(lastName) {
-    let errorState = document.getElementById("error-last-name-input")
-    let classe = document.getElementById("lastName")
-    let regex = new RegExp("^([a-zA-Z])+$");
-    let lastNameTested = regex.test(lastName);
-    if (lastName.trim() === "") {
-        errorState.innerHTML = `This field is required`
-        classe.style.border = '1px solid red';
-    } else if (lastNameTested === false) {
-        errorState.innerHTML = `Please enter a valid last name`
-        classe.style.border = '1px solid red';
-    } else {
-        classe.style.border = '1px solid green';
-        return true
-    }
-}
-
-function verifyEmail(email) {
-    let errorState = document.querySelector(".error-email")
-    let classe = document.getElementById("email")
-    let regex = new RegExp("^[A-Za-z0-9]+@[a-z]+[0-9]*\.+[a-z]+$");
-    let emailTested = regex.test(email)
-    if (email.trim() === "") {
-        errorState.innerHTML = `This field is required`
-        classe.style.border = '1px solid red';
-    } else if (emailTested === false) {
-        errorState.innerHTML = `Please enter a valid email address`
-        classe.style.border = '1px solid red';
-    } else {
-        classe.style.border = '1px solid green';
-        return true
-    }
-}
-
-function verifyRadioChecked(radioInput) {
-    let classeA = document.getElementById("checked-queryType-A")
-    let classeB = document.getElementById("checked-queryType-B")
-    if (radioInput[0].checked === false && radioInput[1].checked === false) {
-        let errorState = document.querySelector(".error-radio")
-        errorState.innerHTML = `Please select a query type`
-    } else if (radioInput[0].checked) {
-        classeA.style.border = '1px solid green';
-        return true
-    } else if (radioInput[1].checked) {
-        classeB.style.border = '1px solid green';
-        return true
-    }
-}
-
-function verifyMessage (message) {
-    let errorState = document.querySelector(".error-message")
-    let classe = document.getElementById("message")
-    if (message.trim() === "") {
-        errorState.innerHTML = `This field is required`
-        classe.style.border = '1px solid red';
-    } else {
-        classe.style.border = '1px solid green';
-        return true
-    }
-}
-
-function verifyCheckboxChecked (checkbox) {
-    let errorState = document.querySelector(".error-checkbox")
-    if (checkbox.checked === false) {
-        errorState.innerHTML = `To submit this form, please consent to being contacted`
-    } else {
-        return true
-    }
-}
-
-function DisplayPopup() {
-    let classe = document.getElementById("popup-container")
-    classe.style.display = "block"
-}
-function ClosePopup() {
-    let classe = document.getElementById("popup-container")
-    classe.style.display = "none"
-}
-
-function verifyAll() {
-    let form = document.querySelector("form")
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
-
-        let firstName = document.getElementById("firstName")
-        let  lastName = document.getElementById("lastName")
-        let email = document.getElementById("email")
-        let radioInput = document.querySelectorAll("input[name='queryType']")
-        let message = document.getElementById("message")
-        let checkbox = document.getElementById("checkbox")
-
-        let firstNameValue = firstName.value
-        let lastNameValue = lastName.value
-        let emailValue = email.value
-        let messageValue = message.value
-
-        verifyFirstName(firstNameValue);
-        verifyLastName(lastNameValue);
-        verifyEmail(emailValue);
-        verifyRadioChecked(radioInput);
-        verifyMessage(messageValue);
-        verifyCheckboxChecked(checkbox);
-
-        if (verifyFirstName(firstNameValue) && verifyLastName(lastNameValue) && verifyEmail(emailValue) && verifyRadioChecked(radioInput) && verifyMessage(messageValue) && verifyCheckboxChecked(checkbox)) {
-            DisplayPopup()
-        }
-    })
-}
-//Appel de la fonction principale
-verifyAll();
+document.querySelector('dialog button').addEventListener(
+    'click', () => {popup.close()}
+)
