484bdbd76a68d877093131cbdbce1574a0d86978
chanwinharold <153543115+chanwinharold@users.noreply.github.com>
2025-05-29 14:02:07 +0000

    size, padding and error message fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,22 +11,28 @@ form.addEventListener('submit', (e)=> {
     const message = document.getElementById('message')
     const checkbox = document.getElementById('checkbox')
 
+    document.querySelector('label[for="checkbox"]').style.color = "black"
     document.querySelectorAll('.error').forEach((span) => span.innerText = "")
+    document.querySelectorAll('#firstName, #lastName, #email, #message').forEach((input) => input.style.borderColor = "black")
     
     let valid = 0
 
     if (firstname.value.trim() == "") {
         document.querySelector('#firstName + span').innerText = "This field cannot be empty"
+        document.querySelector('#firstName').style.borderColor = 'red'
     } else valid++
 
     if (lastname.value.trim() == "") {
         document.querySelector('#lastName + span').innerText = "This field cannot be empty"
+        document.querySelector('#lastName').style.borderColor = 'red'
     } else valid++
 
     if (email.value.trim() == "") {
         document.querySelector('#email + span').innerText = "This field cannot be empty"
+        document.querySelector('#email').style.borderColor = 'red'
     } else if (!email.checkValidity()) {
         document.querySelector('#email + span').innerText = "Please enter a valid email"
+        document.querySelector('#email').style.borderColor = 'red'
     } else valid++
 
     if (!radios[0].checked && !radios[1].checked) {
@@ -35,10 +41,11 @@ form.addEventListener('submit', (e)=> {
 
     if (message.value.trim() == "") {
         document.querySelector('#message + span').innerText = "This field cannot be empty"
+        document.querySelector('#message').style.borderColor = 'red'
     } else valid++
 
     if (!checkbox.checked) {
-        document.querySelector('label[for="checkbox"] > span').innerText = "Please check this field"
+        document.querySelector('label[for="checkbox"]').style.color = "red"
     } else valid++
 
     if (valid ==6) {
